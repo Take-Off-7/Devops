9d08e7d5a990fb32b15b5bb7e37af90d182ceb85
Take-Off-7 <tfakeye7@gmail.com>
2025-11-14 12:43:29 +0000

    Clean repo, ignore Terraform binaries

--- python_for_devops/infrastructure_as_code/terraform/modules/s3/main.tf
+++ python_for_devops/infrastructure_as_code/terraform/modules/s3/main.tf
@@ -5,22 +5,27 @@ resource "random_id" "suffix" {
 
 # S3 bucket
 resource "aws_s3_bucket" "www" {
-  bucket = "www.${var.domain_name}-${random_id.suffix.hex}"
-
+  bucket        = "www.${var.domain_name}-${random_id.suffix.hex}"
   force_destroy = true
+
+  # Website configuration directly on the bucket
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
 }
 
 # Disable S3 Block Public Access for this bucket so public policy works
 resource "aws_s3_bucket_public_access_block" "www" {
   bucket = aws_s3_bucket.www.id
 
-  block_public_acls       = true
-  block_public_policy     = false   # allow public bucket policy
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-# Bucket policy for public read
+# Public read policy for static website
 resource "aws_s3_bucket_policy" "www_policy" {
   bucket = aws_s3_bucket.www.id
 
@@ -29,31 +34,18 @@ resource "aws_s3_bucket_policy" "www_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "AddPerm",
+      "Sid": "PublicReadGetObject",
       "Effect": "Allow",
       "Principal": "*",
-      "Action": ["s3:GetObject"],
-      "Resource": ["arn:aws:s3:::www.${var.domain_name}-${random_id.suffix.hex}/*"]
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::www.${var.domain_name}-${random_id.suffix.hex}/*"
     }
   ]
 }
 POLICY
 }
 
-# Website configuration
-resource "aws_s3_bucket_website_configuration" "www" {
-  bucket = aws_s3_bucket.www.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
-
 # Upload all static files
-locals {
-  static_files = fileset("${path.root}/static_files", "*")
-}
-
 resource "aws_s3_object" "static" {
   for_each = { for f in fileset("${path.root}/static_files", "*") : f => f }
 
